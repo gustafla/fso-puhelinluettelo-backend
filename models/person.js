87a3fa07dce69d481e97d90d65cdf04cd3aab618
Lauri Gustafsson <me@gustafla.space>
2022-10-03 15:20:28 +0300

    Fix mongodb url logging

--- models/person.js
+++ models/person.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to, url')
+console.log('connecting to', url)
 mongoose.connect(url)
   .then(_result => {
     console.log('connected to MongoDB')
